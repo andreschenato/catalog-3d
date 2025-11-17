5aad1b36f21e106a7665d9488906288196e2495a
Andre Schenato <andreschenato@gmail.com>
2025-11-17 20:18:33 -0300

    feat(firebase): add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:catalog_3d/app.dart';
+import 'package:catalog_3d/core/config/firebase.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await initFirebase();
   runApp(const App());
 }
